4cc135cbd0ff92ee105f1ddeeede9178e0ec55c0
Deofajar10 <situmorangdeofajar@gmail.com>
2025-11-29 09:51:56 +0700

    Delete src directory

--- src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const paymentController = require('../controllers/paymentController');
-
-router.post('/booking/:bookingId', auth, paymentController.createPaymentForBooking);
-router.post('/midtrans-notification', paymentController.handleMidtransNotification);
-
-module.exports = router;
